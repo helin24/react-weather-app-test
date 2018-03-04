325db7d94ae840355cc57216196dbe5d53b1e1c2
Helin Shiah <helin.shiah@gmail.com>
2018-03-03 16:49:18 -0800

    Remove today text

--- app/assets/javascripts/components/WeatherFeature.jsx
+++ app/assets/javascripts/components/WeatherFeature.jsx
@@ -2,7 +2,6 @@ class WeatherFeature extends React.Component {
   render() {
     return (
       <div className="weather-feature">
-        <h2>Today </h2>
         <h2>{this.props.date}</h2>
         <h1>{this.props.temp}°</h1>
       </div>
